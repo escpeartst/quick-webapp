df26cfc63bf1d98477b09a9eb4e4543b94ae2438
David Huynh <escpe.artst@gmail.com>
2026-05-07 16:44:39 -0700

    fix responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
           <div><h1 style={{ fontSize: 28, fontWeight: 600 }}>Let there be light...</h1></div>
-          <div style={{ display: 'flex'}}>
-            <div style={{ marginRight: '12px'}}>
-              <Image src={"/IMG_9854.jpeg"} alt={""} width={"400"} height={"800"} style={{ maxWidth: '400px'}}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+            <div >
+              <img src={"/IMG_9854.jpeg"} alt={""} className="w-full h-auto rounded-xl object-cover"
               />
             </div>
             <div>
-              <Image src={"/IMG_9855.jpeg"} alt={""} width={"400"} height={"800"} style={{ maxWidth: '400px'}}
+              <img src={"/IMG_9855.jpeg"} alt={""} className="w-full h-auto rounded-xl object-cover"
               />
             </div>
           </div>
