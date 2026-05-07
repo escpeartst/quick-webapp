9f8544526016a3e5e3142d5ab9357fcbf35216c0
David Huynh <escpe.artst@gmail.com>
2026-05-07 16:47:07 -0700

    Fix padding.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-8 px-8 bg-white dark:bg-black sm:items-start">
           <div><h1 style={{ fontSize: 28, fontWeight: 600 }}>Let there be light...</h1></div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
             <div >
